44f687a847e9e72fcbd9973064a5a7dc5c0ef249
vapasi <vapasi@in-vapasi10.local>
2019-04-26 12:50:06 +0530

    AddingProduct

--- src/test/java/tests/AddingProductTest.java
+++ src/test/java/tests/AddingProductTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,7 +9,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import javax.sql.rowset.BaseRowSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -15,10 +17,10 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 
-public class SimpleLoginTest extends BaseTest {
+public class AddingProductTest extends BaseTest {
 
-    @Test
-    public void testLogin(){
+    /*@Test
+    public void testLogin() {
 
         driver.navigate().to("https://spree-vapasi.herokuapp.com");
         // driver.navigate().to("https://spree-vapasi.herokuapp.com");
@@ -31,6 +33,31 @@ public class SimpleLoginTest extends BaseTest {
 
         WebElement myAccountElement = driver.findElement(By.linkText("My Account"));
         assertEquals("My Account", myAccountElement.getText());
-    }
+    }*/
 
+    @Test
+    public void testaddProduct() {
+        driver.navigate().to("https://spree-vapasi.herokuapp.com");
+        driver.findElement(By.linkText("Ruby on Rails Bag")).click();
+        driver.findElement(By.id("add-to-cart-button")).click();
+
+        assertTrue(driver.findElement(By.linkText("Ruby on Rails Bag")).isDisplayed());
+        assertTrue(driver.findElement(By.id("content")).findElement(By.linkText("Ruby on Rails Bag")).isDisplayed());
+
+        WebElement shoppingCart = driver.findElement(By.id("cart-detail"));
+        List<WebElement> lineItems = shoppingCart.findElements(By.id("line_items"));
+        System.out.println(lineItems.size());
+        assertTrue(lineItems.size() == 1);
+
+        //for (Iterator<WebElement> iterator = lineItems.iterator(); productiterator.hasNext(); ) {
+        // WebElement next =  productiterator.next();
+
+    }
 }
+
+
+
+
+
+
+
---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+
+    ChromeDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        String currentUsersWorkingDir = System.getProperty("user.dir");
+        System.out.println("Dir is " + currentUsersWorkingDir);
+        System.setProperty("webdriver.chrome.driver", currentUsersWorkingDir + "/src/test/resources/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+    @AfterMethod
+    public void tearDown()
+    {
+        driver.close();
+        driver.quit();
+    }
+
+    }
+
+
